8a4aebe436eeef6703d3c7f68859f7c5a64d0132
Nick Greenwood <ngreenwood@tumblr.com>
2014-11-11 15:03:48 -0500

    Added versioning

--- pytumblr/request.py
+++ pytumblr/request.py
@@ -12,10 +12,15 @@ class TumblrRequest(object):
     A simple request object that lets us query the Tumblr API
     """
 
+    __version = "0.0.1";
+
     def __init__(self, consumer_key, consumer_secret="", oauth_token="", oauth_secret="", host="http://api.tumblr.com"):
         self.host = host
         self.consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
         self.token = oauth.Token(key=oauth_token, secret=oauth_secret)
+        self.headers = {
+            "User-Agent" : "pytumblr/" + self.__version
+        }
 
     def get(self, url, params):
         """
@@ -33,7 +38,7 @@ class TumblrRequest(object):
         client = oauth.Client(self.consumer, self.token)
         try:
             client.follow_redirects = False
-            resp, content = client.request(url, method="GET", redirections=False)
+            resp, content = client.request(url, method="GET", redirections=False, headers=self.headers)
         except RedirectLimit, e:
             resp, content = e.args
 
@@ -56,7 +61,7 @@ class TumblrRequest(object):
                 return self.post_multipart(url, params, files)
             else:
                 client = oauth.Client(self.consumer, self.token)
-                resp, content = client.request(url, method="POST", body=urllib.urlencode(params))
+                resp, content = client.request(url, method="POST", body=urllib.urlencode(params), headers=self.headers)
                 return self.json_parse(content)
         except urllib2.HTTPError, e:
             return self.json_parse(e.read())
