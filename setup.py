d9d5e8ab8ac839b7a4bc90020aa1a1a0e9e3875e
Hugo <hugovk@users.noreply.github.com>
2019-02-14 16:34:10 +0200

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     test_suite='nose.collector',
